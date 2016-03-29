c29b2c7804760a1c1610fa5427ce5d0e45fb781d
congyingTech <congyingTech@163.com>
2016-03-29 14:40:00 +0800

    update my company local code.

--- BinaryTreeLevelOrderTraversal.py
+++ BinaryTreeLevelOrderTraversal.py
@@ -8,21 +8,28 @@ class TreeNode:
 class Solution:
     def levelOrder(self, root):
         result = []
-        
-        current = root
-        if current is None:
+        #current结点list一定是一个list类型,因为current不一定只有一个结点
+        current = [root]
+        if root is None:
             return result
         while current:
-            vals = []
-            vals.append(current.val)
-            if current.left:
-                self.levelOrder(current.left)
-            if current.right:
-                current = current.right
-                self.levelOrder(current.right)
+            #vals[] 是记录该层的结点的value的,存的是value值,同时,我们要能够遍历下一层的结点,next_level=[],存的是TreeNode
+            #此处一定要写两个[]
+            vals, next_level = [], []
+            #因为该层的结点可能不止一个,所以要进行for循环
+            for i in current:
+                #将i结点的值加入vals
+                vals.append(i.val)
+                #通过current的左右结点是否存在,得到下一层的结点
+                if i.left:
+                    next_level.append(i.left)
+                if i.right:
+                    next_level.append(i.right)
+            #当前结点加入完毕后,1)将vals加入到result中;2)将current指向下一层
             result.append(vals)
-            print(vals)
+            current = next_level
         return result
+
     
 if __name__ == "__main__":
     root = TreeNode(3)
