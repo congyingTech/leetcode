24ae231d8dffcea0d22fa1334e02b84d50839f01
congyingTech <congyingTech@163.com>
2016-05-03 10:22:30 +0800

    add valid-parentheses

--- valid-parentheses.py
+++ valid-parentheses.py
@@ -4,11 +4,22 @@
 #The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not
 #首先，想到的是每一对括号都要是分拆后都要是偶数个。其次每次出现都要按照一定的次序成对出现。
 
-#算法思想：用dict
+#算法思想：用dict，每一个括号相互匹配，进出栈的问题。
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        
+        #list的初始化， stack = []
+        stack, lookup = [], {"(": ")", "{": "}", "[": "]"}
+        for parenthese in s:
+            if parenthese in lookup:
+                stack.append(parenthese)
+                
+            #1)stack 的 len为0的时候，说明这个字串中并不是括号，所以return False
+            #2)stack里面存的是左括号，出栈的时候（左括号出栈），dict[key] = value 推知，lookup[stack.pop()]得到右括号
+            elif len(stack) == 0 or lookup[stack.pop()] != parenthese:
+                return False
+        #当stack都能够pop完，len为0的时候说明是正确的。
+        return len(stack) == 0
+
+
+if __name__ == "__main__":
+    print(Solution().isValid("([{]})"))
