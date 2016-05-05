e2edf27169150faf7dad1a8f92ebc92b5cdbadbf
congyingTech <congyingTech@163.com>
2016-05-05 14:28:59 +0800

    change

--- MaximumDepthofBT.py
+++ MaximumDepthofBT.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     root.right = TreeNode(3)
     root.left.left = TreeNode(4)    
     
-    print resolution().maxDepth(root)
+    print (resolution().maxDepth(root))
 
           
 
